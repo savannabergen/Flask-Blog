cccb272a945375c483130df96f211df88f46e134
Savanna Bergen <savanna.bergen@gmail.com>
2024-06-26 08:44:27 -0500

    Added Error Handling 404/500

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ def index():
 
 def user(name):
     return render_template("user.html", user_name=name)
+
+# Create Custom Error Page
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
